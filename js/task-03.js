0f324b9bb06ad6fdd30801bdb77e4bb1b193c15a
JorG1990 <jorge  garcia>
2023-05-03 22:24:44 -0500

    task-03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,31 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const insert = document.querySelector('images');
+insert.addEventListener('click', () => {
+  const gallerySelect = document.querySelector('.gallery');
+  subject.insertAdjacentHTML(gallerySelect.value, '<strong>inserted text</strong>');
+});
+
+// const reset = document.querySelector('#reset');
+// reset.addEventListener('click', () => {
+//   document.location.reload();
+const itemList= document.createElement("li")
+const image [
+  {
+    url:'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt:'White and Black Long Fur Cat',
+  };
+
+  {
+    url:'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt:'Orange and White Koi Fish Near Yellow Koi Fish',
+    
+  };
+
+  {
+    url:'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt:'Group of Horses Running',
+    
+  };
+]
