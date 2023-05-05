dd77de07a981c4360d6d98063e9b852ec29f0ee9
JorG1990 <jorge  garcia>
2023-05-04 21:31:14 -0500

    correccion primera parte url

--- js/task-03.js
+++ js/task-03.js
@@ -12,31 +12,19 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
-const insert = document.querySelector('images');
-insert.addEventListener('click', () => {
-  const gallerySelect = document.querySelector('.gallery');
-  subject.insertAdjacentHTML(gallerySelect.value, '<strong>inserted text</strong>');
+const galery = document.querySelector(".gallery") 
+
+images.forEach (({url, alt}) => {
+  const imageElement= `<img url"${url}" alt"${alt}"/>`
+  console.log (imageElement)
 });
 
-// const reset = document.querySelector('#reset');
-// reset.addEventListener('click', () => {
-//   document.location.reload();
-const itemList= document.createElement("li")
-const image [
-  {
-    url:'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt:'White and Black Long Fur Cat',
-  };
+const insert = document.querySelector('images');
+/*insert.addEventListener('click', () => {
+  const gallerySelect = document.querySelector('.gallery');
+  subject.insertAdjacentHTML(gallerySelect.value, '<strong>inserted text</strong>');
+});*/
 
-  {
-    url:'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt:'Orange and White Koi Fish Near Yellow Koi Fish',
-    
-  };
-
-  {
-    url:'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt:'Group of Horses Running',
-    
-  };
-]
+ /*const reset = document.querySelector('#reset');
+reset.addEventListener('click', () => {
+  document.location.reload()*/
