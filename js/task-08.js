a36e7fe220e60c09ef07684cc0664e00ee51b5c1
JorG1990 <jorge  garcia>
2023-05-22 14:41:54 -0500

    task 07,08,09,10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('.login-form');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Evita la recarga de la página
 
+      var elements = form.elements;
+      var formData = {};
+
+      // Verifica si hay espacios en blanco
+      var hasBlankFields = false;
+      for (var i = 0; i < elements.length; i++) {
+        var input = elements[i];
+        if (input.type !== 'submit' && input.value.trim() === '') {
+          hasBlankFields = true;
+          break;
+        }
+        formData[input.name] = input.value;
+      }
+
+      if (hasBlankFields) {
+        alert('Todos los espacios deben ser rellenados.');
+      } else {
+        console.log(formData);
+        form.reset();
+      }
+    });
+  });
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
+
+
+
+/*function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}*/
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+}
+
+document.querySelector('[data-create]').addEventListener('click', function() {
+  const amount = parseInt(document.querySelector('input[type="number"]').value);
+  createBoxes(amount);
+});
+
+document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
